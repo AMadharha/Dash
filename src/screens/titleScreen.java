30dbab8b1c91a2325ed5a515770aa4aa2e1319b4
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-28 00:59:23 -0400

    temp background image added and displays

--- src/screens/titleScreen.java
+++ src/screens/titleScreen.java
@@ -1,9 +1,24 @@
 package src.screens;
 
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class titleScreen {
     public static void create(Stage primaryStage) {
+        Pane root = new Pane();
 
+        Image imgBackground = new Image("./././images/eskimodash-background.png");
+        ImageView ivBackground = new ImageView(imgBackground);
+        root.getChildren().add(ivBackground);
+
+        Scene sceneTitle = new Scene(root, 
+                                     imgBackground.getWidth(),
+                                     imgBackground.getHeight());
+        
+        primaryStage.setScene(sceneTitle);
+        primaryStage.show();
     }
 }
