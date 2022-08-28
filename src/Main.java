f25b0cad0a87064d88562439838c3fb83d8edfca
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-28 00:40:12 -0400

    titleScreen file created

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ package src;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import src.screens.titleScreen;
  
 public class Main extends Application {
     public static void main(String[] args) {
@@ -9,6 +11,6 @@ public class Main extends Application {
     }
     
     public void start(Stage primaryStage) {
-        
+        titleScreen.create(primaryStage);
     }
 }
