3d8eac8ca7a6ffff2d8a8740aee8593822c8e5e5
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-30 16:50:27 -0400

    starting end screen

--- src/screens/endScreen.java
+++ src/screens/endScreen.java
@@ -1,10 +1,11 @@
 package src.screens;
 
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class endScreen {
     public static void createAndSet(Stage primaryStage, int score, TextField tfName) {
-        System.out.println(tfName.getText() + "=" + score);
+        Pane root = new Pane();
     }
 }
