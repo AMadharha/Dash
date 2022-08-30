9316af706a2102e4f34c72463a55bdf8b8d03ceb
Ankush Madharha <ankushmadharha@gmail.com>
2022-08-29 23:24:33 -0400

    game screen code with temp code

---
+++ src/screens/gameScreen.java
@@ -0,0 +1,10 @@
+package src.screens;
+
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class gameScreen {
+    public static void createAndSet(TextField tfName, int characterNum, Stage primaryStage) {
+        System.out.println("test");
+    }
+}
